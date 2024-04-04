df8f00e29d3faf82bd0e6da3ada4c89298f4da33
favian.naufal <favian.naufal@ui.ac.id>
2024-04-04 21:07:01 +0700

    app initialization

---
+++ src/test/java/org/syl/authapp/AuthAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.syl.authapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
